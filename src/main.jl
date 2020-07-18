a4690dd668cc02185f8055e70b964e25e00e5e08
hide-dog <hidee3224@gmail>
2020-07-18 18:26:56 +0900

    k

--- src/main.jl
+++ src/main.jl
@@ -32,6 +32,9 @@ function main()
         cell_Ahat_plas,cell_Ahat_minus,cell_Bhat_plas,cell_Bhat_minus = cal_jacobi(Qbase,Qcon,cellxmax,cellymax,specific_heat_ratio,vecAx,vecAy)
         cell_E_hat_plas,cell_E_hat_minus,cell_F_hat_plas,cell_F_hat_minus = setup_cell_flux_hat(Qcon,cellxmax,cellymax,cell_Ahat_plas,cell_Ahat_minus,cell_Bhat_plas,cell_Bhat_minus)
 
+        println(cell_E_hat_plas[2,2,:])
+        println(cell_E_hat_minus[2,2,:])
+
         E_hat,F_hat = FVS(cell_E_hat_plas,cell_E_hat_minus,cell_F_hat_plas,cell_F_hat_minus,cellxmax,cellymax)
         
         RHS = setup_RHS(cellxmax,cellymax,E_hat,F_hat,Qcon_hat)
